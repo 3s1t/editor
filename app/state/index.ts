7f5dc0857c21b80dcaf94bf3728c0dd7064a99e2
michals2 <lgm1234@protonmail.com>
2022-06-02 12:19:19 -0700

    abstract engineState to it's own file

--- app/state/index.ts
+++ app/state/index.ts
@@ -1,27 +1,12 @@
-import create from "zustand";
-import produce from "immer";
-import { defaultInitialEditorState } from "./defaultState";
-import { getItemAtBreadcrumbs } from "./helpers";
-
 /**
  * Engine
  */
-export interface EngineState {
-  elapsedTime: number;
-  setElapsedTime: (to: number) => void;
+export { useEngineStore } from "./engineState";
+export type { EngineState } from "./engineState";
 
-  crankshaftAngle: number;
-  setCrankshaftAngle: (to: number) => void;
-}
-
-export const useEngineStore = create<EngineState>()((set) => ({
-  elapsedTime: 0,
-  setElapsedTime: (to) => set(() => ({ elapsedTime: to })),
-
-  crankshaftAngle: 0,
-  setCrankshaftAngle: (to) => set(() => ({ crankshaftAngle: to })),
-}));
-
+/**
+ * Editor
+ */
 export { useEditorStore } from "./editorState";
 export type {
   TabState,
